1e680e0068d193e64c5ae5421e2ee8d56aa8737a
Caroline Verticchio <carolineverticchio@Carolines-MacBook-Pro.local>
2019-02-04 16:11:40 -0500

    add upload form

--- requestHandlers.js
+++ requestHandlers.js
@@ -6,13 +6,12 @@ function start(response, postData) {
 
     var body = '<html>' +
         '<head>' +
-            '<meta http-equiv="Content-Type" content="text/html; ' +
-            'charset=UTF-8" />' +
+            '<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />' +
         '</head>' +
         '<body>' +
-            '<form action="/upload" method="post">' +
-                '<textarea name="text" rows="20" cols="60"></textarea>' +
-                '<input type="submit" value="Submit text" />' +
+            '<form action="/upload" enctype="multipart/form-data" method="post">' +
+                '<input type="file" name="upload">' +
+                '<input type="submit" value="Upload file" />'+
             '</form>' +
         '</body>' +
     '</html>';
@@ -25,7 +24,7 @@ function start(response, postData) {
 function upload(response, postData) {
     console.log('Request handler "upload" was called.');
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("You've sent: " + querystring.parse(postData).text);
+    response.write("You've sent the text: " + querystring.parse(postData).text);
     response.end();
 }
 
